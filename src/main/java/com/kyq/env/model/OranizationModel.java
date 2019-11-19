b269804353f321ac3669a8eeb586763767b7e766
hiYuzu <pomelo.yuzu1997@gmail.com>
2019-11-19 09:21:00 +0800

    删除敏感信息

--- src/main/java/com/kyq/env/model/OranizationModel.java
+++ src/main/java/com/kyq/env/model/OranizationModel.java
@@ -1,15 +1,9 @@
-/**
- * Copyright (c) 1996-2016天津通广集团电子信息部，版权所有，复制必究。
- * 此程序版权归天津通广集团电子信息部所有，任何侵犯版权的行为将被追究
- * 法律责任。未经天津通广集团电子信息部的书面批准，不得将此程序的任何
- * 部分以任何形式、采用任何手段、或为任何目的，进行复制或扩散。
- */
 package com.kyq.env.model;
 
 
-import com.kyq.env.util.DefaultArgument;
+ import com.kyq.env.util.DefaultArgument;
 
-public class OranizationModel extends BaseModel{
+public class OranizationModel extends BaseModel {
 	private int orgId = DefaultArgument.INT_DEFAULT;
 	private int orgIdParent = DefaultArgument.INT_DEFAULT;
 	private String orgName;
